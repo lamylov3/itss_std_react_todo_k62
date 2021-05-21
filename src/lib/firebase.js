7ad52153bf38e9f1d7f6a762f70611dc2eeabb17
lamylov3 <ec2-user@ip-172-31-25-215.ec2.internal>
2021-05-21 09:01:13 +0000

    Login by Authentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -12,44 +12,38 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
 
 export const getFirebaseItems = async () => {
   try {
-    const snapshot = await db
-      .collection("todos")
-      .get();
-    const items = snapshot.docs.map(
-      (doc) => ({ ...doc.data(), id: doc.id })
-    );
-    return items;
-  } catch (err) {
-    console.log(err);
-    return [];
-  }
-}
 
-export const addFirebaseItem = async (item) => {
-  try {
-    const todoRef = db.collection("todos");
-    await todoRef.add(item);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-export const updateFirebaseItem = async (item, id) => {
-  try {
-    const todoRef = db.collection("todos").doc(id);
-    await todoRef.update(item);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-export const clearFirebaseItem = async (item) => {
-  const todoRef = db.collection("todos").doc(item.id);
-  await todoRef.delete().then(function () {
-  }).catch(function (err) {
+}).catch(function (err) {
     console.log(err);
   });
 };
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+} 
