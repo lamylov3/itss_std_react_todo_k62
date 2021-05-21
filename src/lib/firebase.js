5ba294d1d33933106d60f9784a40099ee71b8808
lamylov3 <ec2-user@ip-172-31-25-215.ec2.internal>
2021-05-21 08:15:53 +0000

    use firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB2iyhHHrsE0aRfLeWxeR353x7IxvGNtqk",
+    authDomain: "fir-sample-54b76.firebaseapp.com",
+    projectId: "fir-sample-54b76",
+    storageBucket: "fir-sample-54b76.appspot.com",
+    messagingSenderId: "145820301567",
+    appId: "1:145820301567:web:d16d44d417b75e3146b4ca",
+};
+
+firebase.initializeApp(firebaseConfig);
